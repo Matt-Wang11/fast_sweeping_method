958d6b2bb6dbbcba73a37381b479cccc0fd05099
Matt-Wang11 <m4tt_0@icloud.com>
2023-03-04 00:34:25 -0500

    Created button event

--- fsm_gui.py
+++ fsm_gui.py
@@ -11,6 +11,30 @@ def draw_rectangles(s, x, y, p_l, p_g, c=(255, 255, 255)):
                                               p_l + (j * p_l) + (j * p_g), 
                                               p_g, p_g))
 
+def is_in_rectangle(mouse_x, mouse_y, left, top, right, down):
+    return mouse_x > left and mouse_x < right and \
+        mouse_y > top and mouse_y < down
+
+def rectangle_clicked(mouse_x, mouse_y, left, top, p_g, event):
+    return is_in_rectangle(mouse_x, mouse_y, left, top, left + p_g, top + p_g)\
+    and event.type == pygame.MOUSEBUTTONDOWN
+
+def click(s, x, y, mouse_x, mouse_y, p_l, p_g, c, event):
+    coordinates = list
+
+    for i in range(x):
+        for j in range(y):
+            left = p_l + (i * p_l) + (i * p_g)
+            top = p_l + (j * p_l) + (j * p_g)
+
+            if rectangle_clicked(mouse_x, mouse_y, left, top, p_g, event):
+                pygame.draw.rect(surface=s, color=c, 
+                                 rect=pygame.Rect(left, top, p_g, p_g))
+                coordinates = [i, j]
+                break;
+    
+    return coordinates
+
 def main():
     x_len = 50
     y_len = 50
@@ -23,15 +47,16 @@ def main():
         [x_len * pixels_per_grid + (x_len+1) * pixels_per_line,
         y_len * pixels_per_grid + (y_len+1) * pixels_per_line])
 
-    grids = []
-
     is_running = True
     draw_rectangles(screen, x_len, y_len, pixels_per_line, pixels_per_grid)
-    pygame.display.flip()
 
     while is_running:
+        mouse = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
+            click(screen, x_len, y_len, mouse[0], mouse[1], 
+                  pixels_per_line, pixels_per_grid, (0, 255, 0), event)
+            
             if event.type == pygame.QUIT:
                 is_running = False
 
